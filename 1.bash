1db90165e8ececee9d6d2abd666fa7056bcd75db
Kiril B <kirbou06012@gmail.com>
2024-05-27 17:19:35 -0300

    first commit

---
+++ 1.bash
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install x11vnc net-tools
+
+
+FILE = "/etc/systemd/system/x11vnc.service"
+
+printf "[Unit]\nDescription=x11vnc remote desktop server\nAfter=multi-user.target\n[Service]\nType=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/%s/.vnc/passwd -rfbport 5900 -shared\nRestart=on-failure\n[Install]\nWantedBy=multi-user.target" "$USER" >> FILE
+
+
+systemctl daemon-reload
+systemctl start x11vnc
+systemctl enable x11vnc.service
