a5f360f9bac13bb1989c675fb84d8e2e284be100
Kiril B <kirbou06012@gmail.com>
2024-05-27 20:19:14 -0300

    Fixed inital bugs after testing.

--- 1.bash
+++ 1.bash
@@ -1,10 +1,11 @@
 apt-get update
 apt-get install x11vnc net-tools
 
+x11vnc -storepasswd
 
-FILE = "/etc/systemd/system/x11vnc.service"
-
-printf "[Unit]\nDescription=x11vnc remote desktop server\nAfter=multi-user.target\n[Service]\nType=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/%s/.vnc/passwd -rfbport 5900 -shared\nRestart=on-failure\n[Install]\nWantedBy=multi-user.target" "$USER" >> FILE
+touch "/etc/systemd/system/x11vnc.service"
+FILE="/etc/systemd/system/x11vnc.service"
+printf "[Unit]\nDescription=x11vnc remote desktop server\nAfter=multi-user.target\n[Service]\nType=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -loop -noxdamage -repeat -rfbauth /home/%s/.vnc/passwd -rfbport 5900 -shared\nRestart=on-failure\n[Install]\nWantedBy=multi-user.target\n" "$USER" > $FILE
 
 
 systemctl daemon-reload
